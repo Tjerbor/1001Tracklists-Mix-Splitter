c31f2b1601f380e3f3e061698a6e02dd4ef15852
Thiemo <thiemo.ott@gmail.com>
2022-11-17 10:22:25 +0100

    17.11.22 small

--- src/Main.java
+++ src/Main.java
@@ -6,10 +6,14 @@ public class Main {
     public static void main(String[] args) throws IOException {
         supressWarnings();
 
+        Tracklister.shietttt();
+        /*
         Tracklister tracks = new Tracklister("https://www.1001tracklists.com/track/18uj2vdx/john-summit-show-me/index.html");
         String url = "https://www.1001tracklists.com/tracklist/nsgpmht/curbi-tape-06-2021-03-12.html";
         tracks.scrape(url);
         System.out.println("uwu");
+        */
+
 	 //https://www.1001tracklists.com/tracklist/nsgpmht/curbi-tape-06-2021-03-12.html
     }
 
--- src/Tracklister.java
+++ src/Tracklister.java
@@ -1,16 +1,14 @@
 import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
-import com.gargoylesoftware.htmlunit.javascript.host.file.File;
-import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.PrintWriter;
 
 public class Tracklister {
     private String website;
@@ -32,18 +30,36 @@ public class Tracklister {
         webClient.waitForBackgroundJavaScript(8000);
         page = webClient.getPage(url);
         Document doc = Jsoup.parse(page.asXml());
-        Elements tracks = doc.select("#tlp_5921274");
 
 
 //        driver.get(url);
-        writeHTMLFile(doc.html());
+        writeHTMLFile(doc.html(), "wholeSite");
     }
 
-    private static void writeHTMLFile(String input) {
+    public static void shietttt() throws IOException {
+        try {
+            File file = new File("fileName.html");
+            extractData(Jsoup.parse(file));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void extractData(Document doc) {
+        Element parent = doc.getElementById("tlTab");
+        Elements els = parent.children();
+        els.select("id :not(#playerWidget)");
+        System.out.println(els.size());
+        if (els.isEmpty()) {
+            System.out.println("Empty Dipshit");
+        }
+    }
+
+    private static void writeHTMLFile(String input, String fileName) {
         FileWriter fWriter = null;
         BufferedWriter writer = null;
         try {
-            fWriter = new FileWriter("fileName.html");
+            fWriter = new FileWriter(fileName + ".html");
             writer = new BufferedWriter(fWriter);
             writer.write(input);
             writer.newLine(); //this is not actually needed for html files - can make your code more readable though
